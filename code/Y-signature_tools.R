dd5f903e1df1a22511193b5fb080899f8dad9383
8medom <matus.medo@unifr.ch>
2024-06-19 09:36:40 +0200

    Increased bootstrap realizations & added exposureFilterType

--- code/Y-signature_tools.R
+++ code/Y-signature_tools.R
@@ -4,7 +4,7 @@ library(dplyr)
 
 mut_mat <- read.csv("data/data_for_MutationalPatterns.dat", sep = "\t", row.names = 1, header = TRUE)
 cosmic3 <- as.matrix(read.table("../input/COSMIC_v3_SBS_GRCh38_for_MutationalPatterns-RELEVANT.txt", sep = ",", header = TRUE, check.names = FALSE))
-res <- Fit(catalogues = mut_mat, signatures = cosmic3, useBootstrap = TRUE, nboot = 10, nparallel = 1)
+res <- Fit(catalogues = mut_mat, signatures = cosmic3, exposureFilterType = "giniScaledThreshold", useBootstrap = TRUE, nboot = 100, nparallel = 1)
 
 
 res_to_save <- t(res$exposures)
