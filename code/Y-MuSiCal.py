be57ba713bcd28c18715057ae56658d0ba7ff49e
8medom <matus.medo@unifr.ch>
2024-09-10 09:58:12 +0200

    MuSiCal script with selected reference signatures

---
+++ code/Y-MuSiCal.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+import musical          # https://github.com/parklab/MuSiCal/tree/main
+import pandas as pd
+
+
+X = pd.read_csv('data/data_for_MutationalPatterns.dat', index_col = 0, sep = '\t')
+W = pd.read_csv('../input/COSMIC_v3_SBS_GRCh38.txt', sep = '\t', index_col = 0)
+MP_index = pd.read_csv('../input/mut_matrix_order_MutationalPatterns.dat', header = None).squeeze()
+W = W.reindex(MP_index)
+relevant_sigs = pd.read_csv('../input/COSMIC_v3_SBS_GRCh38_for_MutationalPatterns-RELEVANT.txt')
+W = W[relevant_sigs.columns]
+H, model = musical.refit.refit(X, W, method = 'likelihood_bidirectional', thresh = 0.001)   # Likelihood-based sparse NNLS
+H.to_csv('signature_results/MuSiCal-contribution.dat')
